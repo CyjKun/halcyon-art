1838bbaaa01cb2a42cea61d46dc4e857a3142edb
CJ Baquiran <lightningpatapon@gmail.com>
2023-05-09 12:09:43 +0800

    add baseUrl

--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -1,6 +1,6 @@
 import useSWR from 'swr'
 
-const baseURL = "http://localhost:3000/"
+const baseURL = "https://halcyon-art-five.vercel.app/"
 const response = (...args) => fetch(...args).then(res => res.json())
 
 export default function fetcher(endpoint){
